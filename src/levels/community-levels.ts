cf2184ae775ee42dad13766e073fbf009d1d091a
timothy.cao <timothy.cao@bytedance.com>
2026-04-03 15:46:20 -0700

    Add roundabout community level

--- src/levels/community-levels.ts
+++ src/levels/community-levels.ts
@@ -1,6 +1,7 @@
 import { type LevelData } from '@/engine/types';
+import communityLevel1001 from './community-1001';
 
-export const builtInCommunityLevels: LevelData[] = [];
+export const builtInCommunityLevels: LevelData[] = [communityLevel1001];
 
 export function getBuiltInCommunityLevel(id: number): LevelData | undefined {
   return builtInCommunityLevels.find(level => level.id === id);
