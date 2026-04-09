79fe5cb5ad02b57be453d662bf2cf2bedf929d2e
timothy.cao <timothy.cao@bytedance.com>
2026-04-08 20:23:35 -0400

    Remove built-in community levels

--- src/levels/community-levels.ts
+++ src/levels/community-levels.ts
@@ -1,11 +1,6 @@
 import { type LevelData } from '@/engine/types';
-import communityLevel1001 from './community-1001';
-import communityLevel1002 from './community-1002';
 
-export const builtInCommunityLevels: LevelData[] = [
-  communityLevel1001,
-  communityLevel1002,
-];
+export const builtInCommunityLevels: LevelData[] = [];
 
 export function getBuiltInCommunityLevel(id: number): LevelData | undefined {
   return builtInCommunityLevels.find(level => level.id === id);
